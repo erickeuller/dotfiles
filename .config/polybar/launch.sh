fe4db24f1efa979c819edc4fd1e9f236a84a660e
Erick Euller Opelt <erick@opelt.dev>
2021-05-07 11:01:38 -0300

    wifi device as env

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,8 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+export WIFI_DEVICE=$(nmcli device | awk '$2 == "wifi" {print $1}')
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar topbar >>/tmp/polybar1.log 2>&1 & disown
